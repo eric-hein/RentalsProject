5c6b75e13e3dd6261eaa3c7c8414bc0453b73dd7
eric-hein <erichein7@bellsouth.net>
2019-07-24 14:22:00 -0400

    Added information to DAO and fixed Film class

--- src/main/java/com/m3/training/rentals/dao/DAO.java
+++ src/main/java/com/m3/training/rentals/dao/DAO.java
@@ -1,7 +1,48 @@
 package com.m3.training.rentals.dao;
 
-public abstract class DAO<T> implements ICRUD<T> {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public abstract class DAO<T> implements ICRUD<T>, AutoCloseable {
 	 
+	private final String PROPERTIES_PATH = "src/main/resources/queries.properties";
+	private static EntityManagerFactory factory;
+	protected EntityManager em;
+
+	Connection dbConnection;
 	
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+	public void loadProperties() {
+		File file = new File(PROPERTIES_PATH);
+		Properties properties = new Properties();
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			properties.load(fis);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public DAO() throws SQLException {
+		loadProperties();
+		factory = Persistence.createEntityManagerFactory("cityJPADemo");
+		em = factory.createEntityManager();
+	}
+	@Override
+	public void close() throws Exception {
+		em.close();
+		
+	}
+
 	
 }
