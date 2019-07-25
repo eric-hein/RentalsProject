293332381c7e89215d42438f37f59554f9077f83
alumniJohnson07 <brian.gimeno13@gmail.com>
2019-07-25 15:26:58 -0400

    Rename staffDAO variables

--- src/main/java/com/m3/training/rentals/dao/StaffDAO.java
+++ src/main/java/com/m3/training/rentals/dao/StaffDAO.java
@@ -22,11 +22,11 @@ public class StaffDAO extends DAO<Staff> {
 		return Optional.ofNullable(staff);
 	}
 	
-	public Optional<List<Rental>> getStaff() {
+	public Optional<List<Staff>> getStaff() {
 
-		TypedQuery<Rental> query = em.createQuery("SELECT s FROM Staff s ORDER BY s.name", Rental.class);
+		TypedQuery<Staff> query = em.createQuery("SELECT s FROM Staff s ORDER BY s.name", Staff.class);
 
-		List<Rental> rentalsList = query.getResultList();
-		return Optional.ofNullable(rentalsList);
+		List<Staff> staffList = query.getResultList();
+		return Optional.ofNullable(staffList);
 	}
 }
