f26e7ae4d0ee7460e24a75aa4fd97bf2256181e0
CramerAJ <jintao.hang@gmail.com>
2019-07-25 11:28:24 -0400

    are you happy now

--- src/main/java/com/m3/training/rentals/utility/ParseTools.java
+++ src/main/java/com/m3/training/rentals/utility/ParseTools.java
@@ -3,6 +3,7 @@ package com.m3.training.rentals.utility;
 public class ParseTools {
 
 	public static String caseEmail(String email) {
+		if (email==null) return null;
 		StringBuilder sb = new StringBuilder();
 		int indexOfAt = email.indexOf('@');
 		sb.append(email.substring(0, indexOfAt).toUpperCase());
